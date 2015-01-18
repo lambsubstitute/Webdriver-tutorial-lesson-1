d7668b3e3f8860eef21260c5e8f14cf083960bd5
keith.ford1980@googlemail.com <b1lly1sg0d>
2015-01-18 12:41:39 +0000

    cleaning up env
    removed extra code not need to keep confusion minimal

--- new waitr base project with PO/Features/support/env.rb
+++ new waitr base project with PO/Features/support/env.rb
@@ -4,38 +4,24 @@ require 'rubygems'
 require 'rspec/expectations'
 include Test::Unit::Assertions;
 
-file:/C:/Users/keith/RubymineProjects/AwesomeDonations/
 
 
 
 
-profile = Selenium::WebDriver::Firefox::Profile.new
-browser = Watir::Browser.new :firefox, :profile => profile
+
 
 
 Before do
-  if LOCALURL = nil
-    require 'features/support/cucumber_settings'
-  end
-
- #ENV['BASEURL'] = LOCALURL + ":" + LOCALPORT + "/"
-  #browser.speed = fast
+  profile = Selenium::WebDriver::Firefox::Profile.new
+  browser = Watir::Browser.new :firefox, :profile => profile
   @browser = browser
 end
 
 
 at_exit do
-  #browser.close
+  @browser.close
 end
 
-
-# class documentation
-# Allows browser object to be shared across page object classes
-module PageInitializer
-  def initialize(browser)
-    @browser = browser
-  end
-end
 
 
 
